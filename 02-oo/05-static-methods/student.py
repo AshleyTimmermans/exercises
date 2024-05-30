edd9eb91c01c61bb34d16c353c830135adc93421
Ashley Timmermans <ashley.timmermans@student.ucll.be>
2024-05-30 15:44:39 +0200

    oo

--- 02-oo/05-static-methods/student.py
+++ 02-oo/05-static-methods/student.py
@@ -1,2 +1,28 @@
 class Duration:
+    def __init__(self, *, time_in_seconds):
+        self.time_in_seconds = time_in_seconds
     
+    @staticmethod
+    def from_seconds(time):
+        return Duration(time_in_seconds = time)
+    
+    @staticmethod
+    def from_minutes(time):
+        return Duration(time_in_seconds=time * 60)
+    
+    @staticmethod
+    def from_hours(time):
+        return Duration(time_in_seconds=time *3600)
+    
+    @property
+    def seconds(self):
+        return self.__duration_in_seconds
+
+    @property
+    def minutes(self):
+        return self.__duration_in_seconds / 60
+
+    @property
+    def hours(self):
+        return self.__duration_in_seconds / 3600
+
